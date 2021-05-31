4f8c2063112d0728f3995f1242c4d3aec02462ac
sophy8281 <wanjikusophia3@gmail.com>
2021-05-31 20:22:07 +0300

    first commit

---
+++ backend/books/urls.py
@@ -0,0 +1,9 @@
+# backend/books/urls.py.
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = [
+    path('books/', views.BookList.as_view()),
+    path('books/<int:pk>/', views.BookDetail.as_view()),
+]
